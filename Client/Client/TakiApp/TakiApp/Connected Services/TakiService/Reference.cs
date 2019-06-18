09b110db81e62fbb507103495a0ee4e0104f6dc8
Samuel Arbibe <38098325+samuelarbibe@users.noreply.github.com>
2019-06-18 12:03:23 +0300

    Taki 1.0
    First Release of the game, both on windows and android.
    
    minor updated:
    -game history now displays players' Usernames and not IDs.

--- Client/Client/TakiApp/TakiApp/Connected Services/TakiService/Reference.cs
+++ Client/Client/TakiApp/TakiApp/Connected Services/TakiService/Reference.cs
@@ -2798,7 +2798,7 @@ namespace TakiApp.TakiService {
         
         private static System.ServiceModel.EndpointAddress GetEndpointAddress(EndpointConfiguration endpointConfiguration) {
             if ((endpointConfiguration == EndpointConfiguration.BasicHttpBinding_IService)) {
-                return new System.ServiceModel.EndpointAddress("http://10.116.2.90:8733/Design_Time_Addresses/Service/Service/");
+                return new System.ServiceModel.EndpointAddress("http://192.168.0.18:8733/Design_Time_Addresses/Service/Service/");
             }
             throw new System.InvalidOperationException(string.Format("Could not find endpoint with name \'{0}\'.", endpointConfiguration));
         }
